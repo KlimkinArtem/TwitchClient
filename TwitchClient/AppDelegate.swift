95ca02a4335b2027ee2e286df595c00601760341
Artem <artemklimkin755@icloud.com>
2019-12-27 14:41:49 +0700

    Initial Commit

---
+++ TwitchClient/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TwitchClient
+//
+//  Created by Артем Климкин on 27/12/2019.
+//  Copyright © 2019 Artem Klimkin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TwitchClient/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TwitchClient
+//
+//  Created by Артем Климкин on 27/12/2019.
+//  Copyright © 2019 Artem Klimkin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
